e8772be6c6f04520dfa2cd8d567d59c58abd6d00
Dmitry Gusev <dmtrxiii@gmail.com>
2022-04-21 10:04:50 +0300

    User search fragment added

--- app/src/main/java/com/ineedyourcode/githubapiapp/MainActivity.kt
+++ app/src/main/java/com/ineedyourcode/githubapiapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ineedyourcode.githubapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ineedyourcode.githubapiapp.ui.screens.usersearch.UserSearchFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_fragment_container_view, UserSearchFragment())
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/ineedyourcode/githubapiapp/ui/screens/usersearch/UserSearchFragment.kt
@@ -0,0 +1,12 @@
+package com.ineedyourcode.githubapiapp.ui.screens.usersearch
+
+import android.os.Bundle
+import android.view.View
+import com.ineedyourcode.githubapiapp.databinding.FragmentUserSearchBinding
+import com.ineedyourcode.githubapiapp.ui.utils.BaseFragment
+
+class UserSearchFragment: BaseFragment<FragmentUserSearchBinding>(FragmentUserSearchBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
