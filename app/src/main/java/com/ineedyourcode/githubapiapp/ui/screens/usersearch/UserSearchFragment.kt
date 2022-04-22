eafbccb1573e150bd2bca8ba624324064d61ff36
Dmitry Gusev <dmtrxiii@gmail.com>
2022-04-22 10:25:38 +0300

    simple recycler view for user search fragment released

--- app/src/main/java/com/ineedyourcode/githubapiapp/ui/screens/usersearch/UserSearchFragment.kt
+++ app/src/main/java/com/ineedyourcode/githubapiapp/ui/screens/usersearch/UserSearchFragment.kt
@@ -2,17 +2,29 @@ package com.ineedyourcode.githubapiapp.ui.screens.usersearch
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.ineedyourcode.githubapiapp.databinding.FragmentUserSearchBinding
+import com.ineedyourcode.githubapiapp.ui.screens.usersearch.recyclerviewadapter.OnUserSearchItemClickListener
+import com.ineedyourcode.githubapiapp.ui.screens.usersearch.recyclerviewadapter.UserSearchRecyclerViewAdapter
 import com.ineedyourcode.githubapiapp.ui.utils.BaseFragment
 
 class UserSearchFragment :
     BaseFragment<FragmentUserSearchBinding>(FragmentUserSearchBinding::inflate) {
     private val viewModel by viewModels<UserSearchViewModel>()
 
+    private lateinit var userSearchAdapter: UserSearchRecyclerViewAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        userSearchAdapter = UserSearchRecyclerViewAdapter(object : OnUserSearchItemClickListener {
+            override fun onUserSearchItemClickListener(login: String) {
+                Toast.makeText(requireContext(), login, Toast.LENGTH_SHORT).show()
+            }
+        })
 
         viewModel.getLiveData().observe(viewLifecycleOwner) {
             renderData(it)
@@ -26,9 +38,15 @@ class UserSearchFragment :
     private fun renderData(state: UserSearchState) {
         when (state) {
             UserSearchState.UserSearchProgress -> {}
+
             is UserSearchState.UserSearchSuccess -> {
-                binding.tempTextView.text = state.userList.toString()
+                userSearchAdapter.setData(state.userList)
+                binding.userListRecyclerView.apply {
+                    layoutManager = LinearLayoutManager(requireContext())
+                    adapter = userSearchAdapter
+                }
             }
+
             is UserSearchState.UserSearchError -> {
                 Snackbar.make(binding.root, state.error, Snackbar.LENGTH_SHORT).show()
             }
---
+++ app/src/main/java/com/ineedyourcode/githubapiapp/ui/screens/usersearch/recyclerviewadapter/UserSearchRecyclerViewAdapter.kt
@@ -0,0 +1,43 @@
+package com.ineedyourcode.githubapiapp.ui.screens.usersearch.recyclerviewadapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.ineedyourcode.githubapiapp.R
+import com.ineedyourcode.githubapiapp.data.dto.GitHubUserProfileDto
+
+class UserSearchRecyclerViewAdapter(val onItemClickListener: OnUserSearchItemClickListener) :
+    RecyclerView.Adapter<UserSearchRecyclerViewAdapter.UserSearchResultViewHolder>() {
+    private var userList = mutableListOf<GitHubUserProfileDto>()
+
+    fun setData(mUserList: List<GitHubUserProfileDto>) {
+        userList = mUserList as MutableList<GitHubUserProfileDto>
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserSearchResultViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_user_search_recycler_view_item, parent, false)
+        return UserSearchResultViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: UserSearchResultViewHolder, position: Int) {
+        holder.userLogin.text = userList[position].login
+        holder.userAvatar.load(userList[position].avatarUrl)
+        holder.userAvatar.setOnClickListener {
+            onItemClickListener.onUserSearchItemClickListener(userList[position].login)
+        }
+    }
+
+    override fun getItemCount() = userList.size
+
+    inner class UserSearchResultViewHolder(view: View) :
+        RecyclerView.ViewHolder(view) {
+        val userLogin: TextView = itemView.findViewById(R.id.user_login_text_view)
+        val userAvatar: ImageView = itemView.findViewById(R.id.user_avatar_image_view)
+    }
+}
