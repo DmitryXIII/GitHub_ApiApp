1ff36e1578276574a08b5c458b657d132c710979
Dmitry Gusev <dmtrxiii@gmail.com>
2022-04-29 14:31:26 +0300

    DataRepository code refactoring

--- app/src/main/java/com/ineedyourcode/githubapiapp/data/repository/DataRepository.kt
+++ app/src/main/java/com/ineedyourcode/githubapiapp/data/repository/DataRepository.kt
@@ -10,7 +10,6 @@ import com.ineedyourcode.githubapiapp.data.utils.convertGitHubUserEntityToDto
 import com.ineedyourcode.githubapiapp.data.utils.convertGitHubUserRepositoriesEntityListToDto
 import com.ineedyourcode.githubapiapp.data.utils.convertGitHubUserRepositoryEntityToDto
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.kotlin.subscribeBy
 
 private enum class DataSourceType {
     DATA_SOURCE_TYPE_MOCK,
@@ -18,7 +17,7 @@ private enum class DataSourceType {
 }
 
 class DataRepository : IDataRepository {
-    private val dataSourceType: DataSourceType = DataSourceType.DATA_SOURCE_TYPE_MOCK
+    private val dataSourceType: DataSourceType = DataSourceType.DATA_SOURCE_TYPE_RETROFIT
 
     private val dataSource = when (dataSourceType) {
         DataSourceType.DATA_SOURCE_TYPE_MOCK -> {
@@ -31,52 +30,32 @@ class DataRepository : IDataRepository {
     }
 
     override fun searchUser(searchingRequest: String): Single<GitHubUserSearchResultDto> {
-        return Single.create { emitter ->
-            dataSource.searchUser(searchingRequest).subscribeBy(
-                onSuccess = { emitter.onSuccess(convertGitHubSearchResultEntityToDto(it)) },
-                onError = { emitter.onError(it) }
-            )
-        }
+        return dataSource.searchUser(searchingRequest)
+            .map { convertGitHubSearchResultEntityToDto(it) }
     }
 
     override fun getMostPopularUsers(): Single<GitHubUserSearchResultDto> {
-        return Single.create { emitter ->
-            dataSource.getMostPopularUsers().subscribeBy(
-                onSuccess = { emitter.onSuccess(convertGitHubSearchResultEntityToDto(it)) },
-                onError = { emitter.onError(it) }
-            )
-        }
+        return dataSource.getMostPopularUsers()
+            .map { convertGitHubSearchResultEntityToDto(it) }
     }
 
     override fun getGitHubUser(login: String): Single<GitHubUserProfileDto> {
-        return Single.create { emitter ->
-            dataSource.getGitHubUser(login).subscribeBy(
-                onSuccess = { emitter.onSuccess(convertGitHubUserEntityToDto(it)) },
-                onError = { emitter.onError(it) }
-            )
-        }
+        return dataSource.getGitHubUser(login)
+            .map { convertGitHubUserEntityToDto(it) }
     }
 
     override fun getGitHubUserRepositoriesList(
         login: String,
     ): Single<List<GitHubUserRepositoryDto>> {
-        return Single.create { emitter ->
-            dataSource.getGitHubUserRepositoriesList(login).subscribeBy(
-                onSuccess = { emitter.onSuccess(convertGitHubUserRepositoriesEntityListToDto(it)) },
-                onError = { emitter.onError(it) }
-            )
-        }
+        return dataSource.getGitHubUserRepositoriesList(login)
+            .map { convertGitHubUserRepositoriesEntityListToDto(it) }
     }
 
     override fun getGitHubRepository(
         repoOwnerLogin: String,
         repoName: String,
     ): Single<GitHubUserRepositoryDto> {
-        return Single.create { emitter ->
-            dataSource.getGitHubRepository(repoOwnerLogin, repoName).subscribeBy(
-                onSuccess = { emitter.onSuccess(convertGitHubUserRepositoryEntityToDto(it)) },
-                onError = { emitter.onError(it) }
-            )
-        }
+        return dataSource.getGitHubRepository(repoOwnerLogin, repoName)
+            .map { convertGitHubUserRepositoryEntityToDto(it) }
     }
 }
